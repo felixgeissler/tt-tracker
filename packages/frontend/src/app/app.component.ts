d93a0ccfe465a99809df70d472ad21fd2455c222
Felix Geißler <felix.geissler.94+dev@gmail.com>
2023-11-21 00:17:34 +0100

    🧱 Add Angular frontend

---
+++ packages/frontend/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'frontend';
+
+  constructor() {
+    const foo = 1;
+    console.log(foo);
+  }
+}
